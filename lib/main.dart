1656a3ae95b7835c0a21a92a371d6dcd77c20f4b
parthramanuj64 <parthramanuj64@gmail.com>
2022-11-09 18:09:25 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Expenses',
+      title: 'Expense Full App',
       theme: ThemeData(
         primaryColor: Colors.yellow,
       ),
