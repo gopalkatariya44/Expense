6952b0a55d89ba9d39b96c849f738ba197b0201d
Gopal Katariya <55906225+gopalkatariya44@users.noreply.github.com>
2022-11-09 18:12:38 +0530

    Revert "Chnage The Name"

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Expense Full App',
+      title: 'Expenses',
       theme: ThemeData(
         primaryColor: Colors.yellow,
       ),
